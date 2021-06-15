0234081a48946d50f77bc52838ca2b1c8ff9caf0
Michael Goulart <michaelgoulart@hotmail.com>
2021-06-15 10:34:43 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,15 @@
+# Slack-Bot-IBM-Watson
+This BOT for slack uses the IBM Watson A.I for responses.
+
+Tools you need to create this BOT 
+IBM Cloud Account (Free) 
+Lite Plan give you 10,000 API Calls per Month*
+Up to 5 Workspaces
+Up to 100 Intents
+Up to 25 Entities
+*POST /message method calls only
+
+# Wiki 
+https://github.nike.com/mgoul3/Slack-Bot-IBM-Watson/wiki
+
+
---
+++ index.js
@@ -0,0 +1,59 @@
+/ Script might deviate a little of the IBM original script.
+/**
+ * Copyright 2016 IBM Corp. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+require('dotenv').load();
+
+var Botkit = require('botkit');
+var express = require('express');
+var middleware = require('botkit-middleware-watson')({
+  username: process.env.ASSISTANT_USERNAME,
+  password: process.env.ASSISTANT_PASSWORD,
+  workspace_id: process.env.WORKSPACE_ID,
+  url: process.env.ASSISTANT_URL || 'https://gateway.watsonplatform.net/assistant/api',
+  version: '2018-07-10'
+});
+
+// Configure your bot.
+var slackController = Botkit.slackbot();
+var slackBot = slackController.spawn({
+  token: process.env.SLACK_TOKEN
+});
+
+slackController.hears(['.*'],['direct_message', 'direct_mention', 'mention','message_received'], function(bot, message) {
+  slackController.log('Slack message received');
+  middleware.interpret(bot, message, function() {
+    if (message.watsonError) {
+      console.log(message.watsonError);
+      bot.reply(message, message.watsonError.description || message.watsonError.error);
+    } else if (message.watsonData && 'output' in message.watsonData) {
+      bot.reply(message, message.watsonData.output.text.join('\n'));
+    } else {
+      console.log('Error: received message in unknown format. (Is your connection with Watson Conversation up and running?)');
+      bot.reply(message, 'I\'m sorry, but for technical reasons I can\'t respond to your message');
+    }
+  });
+});
+
+slackBot.startRTM();
+
+// Create an Express app
+var app = express();
+var port = process.env.PORT || 5000;
+app.set('port', port);
+app.listen(port, function() {
+  console.log('Client server listening on port ' + port);
+});
